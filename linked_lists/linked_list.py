638bf64e456f137fabc8c32e0192fd68e50f832e
Aditya Jadhav <aditya.jadhav27@gmail.com>
2017-03-30 19:38:55 -0500

    add method to find middle element from the list

--- linked_lists/linked_list.py
+++ linked_lists/linked_list.py
@@ -14,6 +14,16 @@ class LinkedList(object):
         node.set_next(self.head)
         self.head = node
 
+    def find_middle_element(self):
+        """ Move one pointer by one and other pointer by two.
+        When the fast pointer reaches end slow pointer will reach middle of the linked list"""
+        ptr1 = self.head
+        ptr2 = self.head
+        while ptr2 is not None and ptr2.next is not None:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next.next  # will move by 2
+        print("The middle element is [{}] \n".format(ptr1.data));
+
     def traverse(self, head_node):
         if head_node is not None:
             print(head_node.data)
--- linked_lists/main.py
+++ linked_lists/main.py
@@ -4,6 +4,10 @@ def main():
     ll = LinkedList()
     ll.push(24)
     ll.push(21)
+    ll.push(25)
+    ll.push(26)
+    ll.push(30)
+    ll.find_middle_element()
     ll.traverse(ll.head)
 
 if __name__ == '__main__':
